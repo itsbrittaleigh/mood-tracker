1dc256157563b41e9cc9e2f933573401c98e78db
Britta Rodenbeck <cheers@brittarodenbeck.com>
2024-12-14 12:29:50 -0600

    🐛 fix: reorganizing data to display in correct rows/columns

--- src/data/moods.ts
+++ src/data/moods.ts
@@ -11,58 +11,8 @@ export const moods: Mood[] = [
     pleasantnessLevel: 0,
   },
   {
-    name: 'Fuming',
-    energyLevel: 4,
-    pleasantnessLevel: 0,
-  },
-  {
-    name: 'Repulsed',
-    energyLevel: 3,
-    pleasantnessLevel: 0,
-  },
-  {
-    name: 'Disgusted',
-    energyLevel: 2,
-    pleasantnessLevel: 0,
-  },
-  {
-    name: 'Miserable',
-    energyLevel: 1,
-    pleasantnessLevel: 0,
-  },
-  {
-    name: 'Despair',
-    energyLevel: 0,
-    pleasantnessLevel: 0,
-  },
-  {
     name: 'Stressed',
     energyLevel: 5,
-    pleasantnessLevel: 1,
-  },
-  {
-    name: 'Angry',
-    energyLevel: 4,
-    pleasantnessLevel: 1,
-  },
-  {
-    name: 'Worried',
-    energyLevel: 3,
-    pleasantnessLevel: 1,
-  },
-  {
-    name: 'Down',
-    energyLevel: 2,
-    pleasantnessLevel: 1,
-  },
-  {
-    name: 'Lonely',
-    energyLevel: 1,
-    pleasantnessLevel: 1,
-  },
-  {
-    name: 'Desolate',
-    energyLevel: 0,
     pleasantnessLevel: 1,
   },
   {
@@ -71,58 +21,8 @@ export const moods: Mood[] = [
     pleasantnessLevel: 2,
   },
   {
-    name: 'Restless',
-    energyLevel: 4,
-    pleasantnessLevel: 2,
-  },
-  {
-    name: 'Uneasy',
-    energyLevel: 3,
-    pleasantnessLevel: 2,
-  },
-  {
-    name: 'Apathetic',
-    energyLevel: 2,
-    pleasantnessLevel: 2,
-  },
-  {
-    name: 'Tired',
-    energyLevel: 1,
-    pleasantnessLevel: 2,
-  },
-  {
-    name: 'Drained',
-    energyLevel: 0,
-    pleasantnessLevel: 2,
-  },
-  {
     name: 'Surprised',
     energyLevel: 5,
-    pleasantnessLevel: 3,
-  },
-  {
-    name: 'Energized',
-    energyLevel: 4,
-    pleasantnessLevel: 3,
-  },
-  {
-    name: 'Pleasant',
-    energyLevel: 3,
-    pleasantnessLevel: 3,
-  },
-  {
-    name: 'At ease',
-    energyLevel: 2,
-    pleasantnessLevel: 3,
-  },
-  {
-    name: 'Relaxed',
-    energyLevel: 1,
-    pleasantnessLevel: 3,
-  },
-  {
-    name: 'Sleepy',
-    energyLevel: 0,
     pleasantnessLevel: 3,
   },
   {
@@ -131,34 +31,34 @@ export const moods: Mood[] = [
     pleasantnessLevel: 4,
   },
   {
-    name: 'Optimistic',
-    energyLevel: 4,
-    pleasantnessLevel: 4,
-  },
-  {
-    name: 'Hopeful',
-    energyLevel: 3,
-    pleasantnessLevel: 4,
-  },
-  {
-    name: 'Content',
-    energyLevel: 2,
-    pleasantnessLevel: 4,
-  },
-  {
-    name: 'Restful',
-    energyLevel: 1,
-    pleasantnessLevel: 4,
-  },
-  {
-    name: 'Tranquil',
-    energyLevel: 0,
-    pleasantnessLevel: 4,
-  },
-  {
     name: 'Ecstatic',
     energyLevel: 5,
     pleasantnessLevel: 5,
+  },
+  {
+    name: 'Fuming',
+    energyLevel: 4,
+    pleasantnessLevel: 0,
+  },
+  {
+    name: 'Angry',
+    energyLevel: 4,
+    pleasantnessLevel: 1,
+  },
+  {
+    name: 'Restless',
+    energyLevel: 4,
+    pleasantnessLevel: 2,
+  },
+  {
+    name: 'Energized',
+    energyLevel: 4,
+    pleasantnessLevel: 3,
+  },
+  {
+    name: 'Optimistic',
+    energyLevel: 4,
+    pleasantnessLevel: 4,
   },
   {
     name: 'Excited',
@@ -166,9 +66,59 @@ export const moods: Mood[] = [
     pleasantnessLevel: 5,
   },
   {
+    name: 'Repulsed',
+    energyLevel: 3,
+    pleasantnessLevel: 0,
+  },
+  {
+    name: 'Worried',
+    energyLevel: 3,
+    pleasantnessLevel: 1,
+  },
+  {
+    name: 'Uneasy',
+    energyLevel: 3,
+    pleasantnessLevel: 2,
+  },
+  {
+    name: 'Pleasant',
+    energyLevel: 3,
+    pleasantnessLevel: 3,
+  },
+  {
+    name: 'Hopeful',
+    energyLevel: 3,
+    pleasantnessLevel: 4,
+  },
+  {
     name: 'Blissful',
     energyLevel: 3,
     pleasantnessLevel: 5,
+  },
+  {
+    name: 'Disgusted',
+    energyLevel: 2,
+    pleasantnessLevel: 0,
+  },
+  {
+    name: 'Down',
+    energyLevel: 2,
+    pleasantnessLevel: 1,
+  },
+  {
+    name: 'Apathetic',
+    energyLevel: 2,
+    pleasantnessLevel: 2,
+  },
+  {
+    name: 'At ease',
+    energyLevel: 2,
+    pleasantnessLevel: 3,
+  },
+  {
+    name: 'Content',
+    energyLevel: 2,
+    pleasantnessLevel: 4,
   },
   {
     name: 'Fulfilled',
@@ -176,9 +126,59 @@ export const moods: Mood[] = [
     pleasantnessLevel: 5,
   },
   {
+    name: 'Miserable',
+    energyLevel: 1,
+    pleasantnessLevel: 0,
+  },
+  {
+    name: 'Lonely',
+    energyLevel: 1,
+    pleasantnessLevel: 1,
+  },
+  {
+    name: 'Tired',
+    energyLevel: 1,
+    pleasantnessLevel: 2,
+  },
+  {
+    name: 'Relaxed',
+    energyLevel: 1,
+    pleasantnessLevel: 3,
+  },
+  {
+    name: 'Restful',
+    energyLevel: 1,
+    pleasantnessLevel: 4,
+  },
+  {
     name: 'Balanced',
     energyLevel: 1,
     pleasantnessLevel: 5,
+  },
+  {
+    name: 'Despair',
+    energyLevel: 0,
+    pleasantnessLevel: 0,
+  },
+  {
+    name: 'Desolate',
+    energyLevel: 0,
+    pleasantnessLevel: 1,
+  },
+  {
+    name: 'Drained',
+    energyLevel: 0,
+    pleasantnessLevel: 2,
+  },
+  {
+    name: 'Sleepy',
+    energyLevel: 0,
+    pleasantnessLevel: 3,
+  },
+  {
+    name: 'Tranquil',
+    energyLevel: 0,
+    pleasantnessLevel: 4,
   },
   {
     name: 'Serene',
