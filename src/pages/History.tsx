e0bbfd76fbc8ea1a64202adde5918ef5c5bed0b1
Britta Rodenbeck <cheers@brittarodenbeck.com>
2024-12-14 10:31:18 -0600

    ✨ feat: displaying timestamped moods on history page

--- src/pages/History.tsx
+++ src/pages/History.tsx
@@ -1,10 +1,24 @@
 import Header from '../components/Header';
+import { localStorageToken } from '../consts';
+import { MoodHistoryItem } from '../types';
 
-const History = () => (
-  <>
-    <Header />
-    <p>history</p>
-  </>
-);
+const History = () => {
+  const historyItems = localStorage.getItem(localStorageToken);
+  const parsedItems = JSON.parse(historyItems ?? '""');
+  const moodHistory: MoodHistoryItem[] = parsedItems.moodHistory ?? [];
+
+  return (
+    <>
+      <Header />
+      <ul>
+        {moodHistory.map(({ datetime, moods }) => (
+          <li key={datetime.toLocaleString()}>
+            {datetime.toLocaleString()}: {moods.join(', ')}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+};
 
 export default History;
