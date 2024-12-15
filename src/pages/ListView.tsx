4e351a85e5bd1c0ecce3da7fd5f632f0a9fb2139
Britta Rodenbeck <cheers@brittarodenbeck.com>
2024-12-15 16:29:15 -0600

    ✨ feat: displaying list view as accordion
    displaying mood logs by date descending within accordions

--- src/pages/ListView.tsx
+++ src/pages/ListView.tsx
@@ -1,30 +1,57 @@
 import styled from 'styled-components';
-import { MoodHistoryItem } from '../types';
+import { MoodHistoryItem, TransformedHistory } from '../types';
 import { getDate, getTime } from '../utils';
-
-const List = styled.ul`
-  padding: 0 14px;
-`;
 
 interface ListViewProps {
   moodHistory: MoodHistoryItem[];
 }
 
+const CollapsibleItem = styled.details`
+  margin-bottom: 1rem;
+`;
+
 const ListView = ({ moodHistory }: ListViewProps) => {
   if (moodHistory.length === 0) return <p>No history.</p>
+  
+  const transformedHistory = moodHistory
+    .reduce((acc, curr) => {
+      const date = getDate(curr.datetime);
+      const time = getTime(curr.datetime);
 
-  return (
-    <List>
-      {moodHistory.map(({ datetime, moods }) => (
-        <li key={datetime.toString()}>
-          <strong>
-            {getDate(datetime)} {getTime(datetime)}  
-          </strong>
-          : {moods.join(', ')}
-        </li>
-      ))}
-    </List>
+      if (!acc[date]) acc[date] = {};
+      if (!acc[date][time]) acc[date][time] = [];
+
+      curr.moods.map((mood) => acc[date][time].push(mood));
+
+      return acc;
+    }, 
+    {} as TransformedHistory,
   );
+
+  const sortedHistory: TransformedHistory = {};
+  Object.keys(transformedHistory)
+    .sort((a, b) => new Date(b).getTime() - new Date(a).getTime())
+    .forEach((date) => {
+      sortedHistory[date] = transformedHistory[date];
+    });
+
+  return Object.entries(sortedHistory).map(([date, dateData], index) => (
+    <CollapsibleItem key={date} open={index === 0}>
+      <summary>{date}</summary>
+      <ul>
+        {Object.entries(dateData).map(([datetime, moods]) => (
+          <li key={datetime}>
+            {datetime}
+            <ul>
+              {moods.map((mood) => (
+                <li key={mood}>{mood}</li>
+              ))}
+            </ul>
+          </li>
+        ))}
+      </ul>
+    </CollapsibleItem>
+  ));
 };
 
 export default ListView;
--- src/types.ts
+++ src/types.ts
@@ -2,3 +2,9 @@ export interface MoodHistoryItem {
   datetime: Date;
   moods: string[];
 }
+
+export interface TransformedHistory {
+  [date: string]: {
+    [datetime: string]: string[];
+  };
+};
